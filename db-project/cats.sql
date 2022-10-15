db053e107061428bc8f07b6333623aaefd2f3b53
cualta <dg.claudiahernandez@gmail.com>
2022-10-14 22:15:27 -0400

    practice create web server basic

--- db-project/cats.sql
+++ db-project/cats.sql
@@ -1,4 +1,4 @@
-/* sqlite3 < cats.sql */  
+/* mysql < cats.sql */  
 CREATE TABLE cats(CatName VARCHAR(20), FavSnack VARCHAR(20), primary key (CatName));
 INSERT INTO cats(CatName, FavSnack) VALUES ('Tesla', 'Ham');
 .backup cats.db
