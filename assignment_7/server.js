95567861a9b3bdaf83ebf9054d5e64d0daa950ad
cualta <dg.claudiahernandez@gmail.com>
2022-10-17 23:25:00 -0400

    post information

--- assignment_7/server.js
+++ assignment_7/server.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const infoRouter = require("./routes/students");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use("/infoRouter", infoRouter);
 
-const host = "localhost";
-const port = 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
+const host = "localhost";
+const port = 8000;
+
+app.get("/", (req, res) => {
   //res.send("Hello world");
+  const students = [
+    {
+      name: "Claudia",
+      course: "DB",
+      registerAt: Date.now(),
+    },
+  ];
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  //res.send("thanks" + req.body.name);
+  var name = req.body.name;
+  var course = req.body.course;
+  res.send("Thanks " + name + ", you are now inscrit to  " + course);
 });
 
 app.listen(port, host, () => {
